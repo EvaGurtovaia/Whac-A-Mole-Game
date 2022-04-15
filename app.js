d801809ba9fc4eaed0d2a9100bccfc71f7a482b9
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-14 19:07:11 -0500

    Adding some js logic and some styling

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const squares = document.querySelectorAll(".squares");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
 
+function randomSquare() {
+    squares.forEach((square) => {
+        square.classList.remove("mole");
+    });
+}
