f2a235d7a5fb85d4a17e5debe87b7bcf2bf5d5c9
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-14 19:49:31 -0500

    Start button  is added and it's working

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const squares = document.querySelectorAll(".squares");
+const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
+const button = document.querySelector(".button");
+
+let result = 0;
+let hitPosition;
 
 function randomSquare() {
     squares.forEach((square) => {
         square.classList.remove("mole");
     });
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    console.log(randomSquare);
+    randomSquare.classList.add("mole");
+
+    hitPosition = randomSquare.id;
 }
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+        if (square.id === hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    });
+});
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 500);
+}
+
+button.addEventListener("click", moveMole);
